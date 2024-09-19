375339d4d4823504ab72f424fc4ad32cfe746726
GUILLERMO OCHOA JIMENEZ <Guillermo.Ochoa@ibm.com>
2024-09-18 18:30:23 -0600

    SWCE-001 Tarea 1 Formulario de registro con correo electronico

--- src/components/inicio_sesion/inicio_sesion.jsx
+++ src/components/inicio_sesion/inicio_sesion.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './inicio_sesion.css';
 
 const InicioS = () => {
+
+    const [action, seAction] = useState("Iniciar Sesion");
+
     return (
         <div className='container'>
             <div className='header'>
-                <div className='textTitle'>Login Edu-Web</div>
+                <div className='textTitle'>{action} Edu-Web</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
@@ -18,8 +21,8 @@ const InicioS = () => {
             </div>
             <div className="forgot-password">¿Olvidaste tu contraseña? <span>Clik aqui</span></div>
             <div className="submit-container">
-                <div className="submit">Registrarse</div>
-                <div className="submit">Iniciar Sesion</div>
+                <div className={action == "Iniciar Sesion"?"submit gray":"submit"} onClick={()=>{seAction("Registro")}}>Registrarse</div>
+                <div className={action == "Registro"?"submit gray":"submit"} onClick={()=>{seAction("Iniciar Sesion")}}>Iniciar Sesion</div>
             </div>
         </div>
     )
